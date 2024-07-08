af1d09ec79557515e8c086c226fec0eded9e7641
Philo <philoatefdev@gmail.com>
2024-07-08 20:11:59 +0300

    creating class employee and developing it

---
+++ Assignment7/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    public enum SecurityLevel
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+    }
+
+    public enum Gender
+    {
+        M,
+        F
+    }
+
+    public class HiringDate
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+
+        public HiringDate(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+    }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public HiringDate HireDate { get; set; }
+        public Gender Gender { get; set; }
+        public SecurityLevel SecurityLevel { get; set; }
+
+        public Employee(int id, string name, Gender gender, SecurityLevel securityLevel, double salary, HiringDate hireDate)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Gender: {Gender}, Security Level: {SecurityLevel}, Salary: {String.Format("{0:C}", Salary)}, Hire Date: {HireDate}";
+        }
+
+    }
+}
